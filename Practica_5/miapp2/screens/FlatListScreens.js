83abb9499d5327fe9a7725b2fefcb2d68a3d8207
Daniellicea <123047951@upq.edu.mx>
2026-06-30 16:43:27 -0600

    ImageBackgroung & SlapshScreen

--- Practica_5/miapp2/screens/FlatListScreens.js
+++ Practica_5/miapp2/screens/FlatListScreens.js
@@ -1,68 +1,58 @@
-/* Zona1: Importaciones componentes y archivos */
-import { View, Text, StyleSheet, FlatList, SectionList, ScrollView} from 'react-native';
+import { FlatList, SectionList, View, StyleSheet, Text} from 'react-native';
 
-/* Zona2: Main - Componentes*/
-export default function FlatListScreen() {
 
-  const frutas = [
-    { id: '1', nombre: 'Manzana' },
-    { id: '2', nombre: 'Plátano' },
-    { id: '3', nombre: 'Naranja' },
-    { id: '4', nombre: 'Mango' },
-  ];
+export default function FlatListScreens(){
 
-  const tareas = [
-    { titulo: 'Mañana', data: ['Hacer ejercicio', 'Desayunar'] },
-    
-    { titulo: 'Tarde', data: ['Estudiar', 'Hacer tarea'] },
-  ];
+    const frutas = [
+        {id:'1', nombre:'Manzana'},
+        {id:'2', nombre:'Papaya'},
+        {id:'3', nombre:'Uvas '},
+        {id:'4', nombre:'Guayaba'},
+    ];
 
-  return (
-    <ScrollView style={ styles.container }>
 
-      <Text style={ styles.titulo }>FlatList</Text>
-        <FlatList
-          data={ frutas }
-          keyExtractor={ (item) => item.id }
-          scrollEnabled={ false }
-          renderItem={ ( { item } ) => (
+    const tareas = [
+      { titulo: 'dia', data:['Desayunar', 'Hacer ejercico']},
+      { titulo: 'Tarde', data:['Estudiar','Hacer la tarea']},
+      { titulo:'Noche' , data:['Estudiar', 'Dormir']}
+    ];
 
-            <View style={ styles.tarjeta }>
-              <Text style={ styles.texto }> { item.nombre }</Text>
-            </View>
-            )
-          }
-        />
+    return(
+      <View style={styles.container}>
+
+      <Text style = {styles.titulo}>FlatList</Text>
+        
+      <FlatList 
+        data={frutas}
+        keyExtractor={(item) => item.id}
+        renderItem={ ({item}) => (
+          <View style= {styles.tarjeta}>
+            <Text style={styles.texto}>{item.nombre}</Text>
+          </View>
+        )}  
+      />
 
       <Text style={styles.titulo}>SectionList</Text>
-        <SectionList
+      <SectionList
+      sections={tareas}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({item}) => (
+        <View style = {styles.tarjeta}>
+          <Text style={styles.seccionTitulo}>{item}</Text>
+        </View>
+      )}
+      renderSectionHeader={({ section }) => (
+        <Text style={styles.seccionTitulo}>{section.titulo}</Text>
+      )}
+      />
+      </View>
+    );
+  }
 
-          sections={ tareas }
-          keyExtractor={ (item , index) => index.toString() }
-          scrollEnabled={false}
-          
-          renderItem={ ( { item } ) => (
-            <View style={ styles.tarjeta }>
-              <Text style={ styles.texto }>{ item }</Text>
-            </View>
-            )
-          }
-
-          renderSectionHeader={ ( { section } ) => (
-            <Text style= { styles.seccionTitulo } > { section.titulo } </Text>
-            )
-         }
-        />
-
-    </ScrollView>
-  );
-}
-
-/* Zona3: Estilos y Posicionamientos*/
-const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#fff', padding: 10 },
-  titulo: { fontSize: 18, fontWeight: '700', backgroundColor: '#111', color: '#fff', padding: 6, marginTop: 10 },
-  seccionTitulo: { fontSize: 15, fontWeight: '700', backgroundColor: '#e9ecef', padding: 6, marginTop: 6 },
-  tarjeta: { backgroundColor: '#f4f4f4', padding: 10, marginBottom: 6, borderRadius: 6 },
-  texto: { fontSize: 14, color: '#222' },
-});
+  const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: '#fff', padding: 10, width: '100%'},
+  titulo: { fontSize: 18, fontWeight: '700', backgroundColor: '#111', color: '#fff', padding: 6, marginTop: 10},
+  seccionTitulo: {fontSize: 15, fontWeight: '700', backgroundColor: '#e9ecef', padding: 6, marginTop: 6},
+  tarjeta: {backgroundColor: '#f4f4f4', padding: 10, marginBottom: 6, borderRadius: 6},
+  texto: {fontSize: 14, color: '#222'}
+})
